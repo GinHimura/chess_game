569ee86fde2a24f952ff0b1f309dba5eadec5a4a
Gerson dos Reis de Melo <gerson.jp.otaku@gmail.com>
2022-02-24 17:27:13 -0300

    Special move - Promotion

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import chess.ChessMatch;
